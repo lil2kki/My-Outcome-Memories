b2d40e4bc36620166ce7a82b21d08bcf0c1aea80
lil2kki <latterrarity70@gmail.com>
2026-08-03 10:44:44 +0300

    Change default name from 'You' to 'LocalPlayer'

---
+++ Anon/main.lua
@@ -0,0 +1,31 @@
+_G.OMAnon_Name         = _G.OMAnon_Name or "LocalPlayer"
+_G.OMAnon_DisplayName  = _G.OMAnon_DisplayName or "LocalPlayer"
+
+function repl(label)
+    if not label:IsA("TextLabel") and not label:IsA("TextButton") then return end
+    if not label.Parent then return end
+    local text = label.Text
+    if string.find(text, game.Players.LocalPlayer.DisplayName) or string.find(text, game.Players.LocalPlayer.Name) then
+        --warn("OMAnon repl at ", label:GetFullName())
+        text = text:gsub(game.Players.LocalPlayer.Name, _G.OMAnon_Name)
+        text = text:gsub(game.Players.LocalPlayer.DisplayName, _G.OMAnon_DisplayName)
+        label.Text = text
+    end
+end
+
+function onDescendantAdded(label)
+    if not label:IsA("TextLabel") and not label:IsA("TextButton") then return end
+    if not label.Parent then return end
+    repl(label)
+    label:GetPropertyChangedSignal("Text"):Connect(function() repl(label) end)
+end
+
+for _, v in ipairs(game.Players.LocalPlayer.PlayerGui:GetDescendants()) do onDescendantAdded(v) end
+game.Players.LocalPlayer.PlayerGui.DescendantAdded:Connect(onDescendantAdded)
+
+for _, v in ipairs(workspace.Lobby:GetDescendants()) do 
+    if v.Name == "skibidi board" then 
+        v.DescendantAdded:Connect(repl) 
+        for _, a in ipairs(v:GetDescendants()) do repl(a) end
+    end
+end
