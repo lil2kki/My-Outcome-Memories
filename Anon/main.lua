a83bd21d388e68afc31be3409d651aab88bd2294
lil2kki <latterrarity70@gmail.com>
2026-08-04 12:42:42 +0300

    Update main.lua

--- Anon/main.lua
+++ Anon/main.lua
@@ -3,12 +3,25 @@ _G.OMAnon_DisplayName  = _G.OMAnon_DisplayName or "LocalPlayer"
 
 print("[OMAnon] Now loading... Made by lil2kki <3")
 
+function filter(label)
+    if not label:IsA("TextLabel") and not label:IsA("TextButton") then return false end
+    if not label.Parent then return false end
+    local path = label:GetFullName()
+    if path:find("skibidi board") then return true end
+    if path:find("FUNPanel.Frame.ScrollingFrame.EX.TextButton") then return true end
+    if path:find("FUNPanel.Frame.targ") then return true end
+    if path:find(".Teamname") then return true end
+    if path:find(".user") then return true end
+    return false
+end
+
 function repl(label)
-    if not label:IsA("TextLabel") and not label:IsA("TextButton") then return end
-    if not label.Parent then return end
+    if not filter(label) then return end
     local text = label.Text
     if string.find(text, game.Players.LocalPlayer.DisplayName) or string.find(text, game.Players.LocalPlayer.Name) then
-        --warn("OMAnon repl at ", label:GetFullName())
+        if not string.find(label:GetFullName(), "skibidi board") then
+            warn("OMAnon repl at ", label:GetFullName())
+        end
         text = text:gsub(game.Players.LocalPlayer.Name, _G.OMAnon_Name)
         text = text:gsub(game.Players.LocalPlayer.DisplayName, _G.OMAnon_DisplayName)
         label.Text = text
@@ -16,8 +29,7 @@ function repl(label)
 end
 
 function onDescendantAdded(label)
-    if not label:IsA("TextLabel") and not label:IsA("TextButton") then return end
-    if not label.Parent then return end
+    if not filter(label) then return end
     repl(label)
     label:GetPropertyChangedSignal("Text"):Connect(function() repl(label) end)
 end
