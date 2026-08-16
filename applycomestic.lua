19e0ca6190797b59ccac6058568e8af7afa64a83
lil2kki <latterrarity70@gmail.com>
2026-08-16 12:07:21 +0300

    Update applycomestic.lua

--- applycomestic.lua
+++ applycomestic.lua
@@ -2,7 +2,7 @@
 -- applycomestic(player, player.OverlayModel)
 
 if not UnlockModule then function UnlockModule(a) print("Executor doesn't need to unlock module", a:GetFullName()) end end
-if not LockModulke then function LockModulke(a) print("Executor doesn't need to unlock module", a:GetFullName()) end end
+if not LockModule then function LockModulke(a) print("Executor doesn't need to unlock module", a:GetFullName()) end end
 
 function applycomestic(player, primaryModel)
 
