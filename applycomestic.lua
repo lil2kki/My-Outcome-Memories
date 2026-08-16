224291f7717232543bf6e550b013bf42d29b26a0
lil2kki <latterrarity70@gmail.com>
2026-08-16 12:04:40 +0300

    Update applycomestic.lua

--- applycomestic.lua
+++ applycomestic.lua
@@ -1,8 +1,8 @@
 -- local applycomestic = loadstring(game:HttpGet("https://github.com/lil2kki/My-Outcome-Memories/raw/HEAD/applycomestic.lua"))()
 -- applycomestic(player, player.OverlayModel)
 
-if not UnlockModule then function UnlockModule(a) print("Executor doesn't need to unlock module") end end
-if not LockModulke then function LockModulke(a) print("Executor doesn't need to unlock module") end end
+if not UnlockModule then function UnlockModule(a) print("Executor doesn't need to unlock module", a:GetFullName()) end end
+if not LockModulke then function LockModulke(a) print("Executor doesn't need to unlock module", a:GetFullName()) end end
 
 function applycomestic(player, primaryModel)
 
